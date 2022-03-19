a14823b0f2cf653a303d6cb71dbda86e3636bd77
slrslrr2 <slrslrr1@gmail.com>
2022-03-19 22:51:41 +0900

    HandlerMapping 0 = RequestMappingHandlerMapping       : 애노테이션 기반의 컨트롤러인         @RequestMapping에서 사용 1 = BeanNameUrlHandlerMapping       : 스프링 빈의 이름으로 핸들러를 찾는다.
    HandlerAdapter
    0 = RequestMappingHandlerAdapter
          : 애노테이션 기반의 컨트롤러인
            @RequestMapping에서 사용
    
    1 = HttpRequestHandlerAdapter
         : HttpRequestHandler 처리
    
    2 = SimpleControllerHandlerAdapter
          : Controller 인터페이스
            (애노테이션X, 과거에 사용) 처리

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,50 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+@Component("/springmvc/old-contrller")
+위 방식은 예전 방식이니 참고 바람!
+
+스프링 빈의 이름으로 Url을 매핑할 수 있다.
+ url을 매핑하기 위해선,
+ 즉, DispatcherServlet이 해당 Url에 지정된 Controller를 찾기 위해선 2가지가 필요하다
+ 1. HandlerMapping
+    @RequstMapping으로 등록 한 경우
+        RequestMappingHandlerMapping
+    @Component로 빈이름을 통해 경로를 등록한 경우  O
+        BeanNameUrlHandlerMapping
+ 2. HandlerAdapter
+    @RequestMapping으로 등록한 경우
+        RequestMappingHandlerAdapter
+    HttpRequestHandler
+        HttpRequestHandlerAdapter
+    SimpleControllerHandlerAdapter O
+        위 소스 일부에서는 아래와 같이 ModelAndView를 반환하는 함수인 handle에
+        아래 상속받은 implements Controller의 handleRequest를 실행 해 주는 부분이 존재한다.
+            public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                return ((Controller) handler).handleRequest(request, response);
+            }
+*/
+
+/**
+ * 정리
+ * org.springframework.web.servlet.mvc.Controller를 상속받은 경우
+ * HandlerMapping : BeanNameUrlHandlerMapping
+ * HandlerAdapter : SimpleControllerHandlerAdapter
+ */
+
+@Component("/sprimgmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
